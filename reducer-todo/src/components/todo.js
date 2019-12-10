0246352ac8d28af92e9f6364e2db942d54935f43
Regino P <regino5298@gmail.com>
2019-12-10 15:44:59 -0700

    added todo file/ after save

---
+++ reducer-todo/src/components/todo.js
@@ -0,0 +1,15 @@
+import React, { useState, useReducer } from 'react';
+import { initialState, reducer } from '../reducers/reducerApp';
+import TFormList from './TFormList'
+
+const Todo = () => {
+
+  return (
+    <div>
+      <TFormList />
+    </div>
+  )
+}
+
+
+export default Todo;
